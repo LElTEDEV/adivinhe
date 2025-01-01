3dd1b917977ff99c655a3ad7572fdbffbf7ca7b6
LElTEDEV <zinenizi@gmail.com>
2025-01-01 14:26:03 -0300

    arrumado o main do app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header"
 
 import styles from './app.module.css'
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export function App() {
 
@@ -15,6 +16,14 @@ export function App() {
         <Header current={8} max={10} onRestart={handleRestartGame}/>
         
         <Tip tip="Uma das linguagens de programação dinâmica mais utilizada"/>
+
+        <div className={styles.word}>
+          <Letter value="R"/>
+          <Letter value="E"/>
+          <Letter value="A"/>
+          <Letter value="C"/>
+          <Letter value="T"/>
+        </div>
       </main>
     </div>
   )
