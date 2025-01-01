6a6533b4be95fd0d3267b136c5846f09a69a9c61
LElTEDEV <zinenizi@gmail.com>
2025-01-01 14:16:09 -0300

    Adicionado o componante de dica TIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header"
 
 import styles from './app.module.css'
+import { Tip } from "./components/Tip"
 
 export function App() {
 
@@ -12,6 +13,8 @@ export function App() {
     <div className={styles.container}>
       <main>
         <Header current={8} max={10} onRestart={handleRestartGame}/>
+        
+        <Tip tip="Uma das linguagens de programação dinâmica mais utilizada"/>
       </main>
     </div>
   )
